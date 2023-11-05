17f03a4d1e3b29322923d2ffee4fb141041d0a15
tehbotolbayu <bayuuabdur2903@gmail.com>
2023-11-05 11:26:28 +0800

    delete

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express"),
-    router = Router(),
-    profileRouter = require('./profileRoutes'),
-    contentRouter = require('./contentRoutes');
-
-router.use('/profile', profileRouter);
-router.use('/content', contentRouter);
-
-module.exports = router;
